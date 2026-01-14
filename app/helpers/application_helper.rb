61139ad2a3f4e6640255e3424c34a31301b0d65d
Blitzkriegtech <aljunrendon@yahoo.com>
2026-01-14 09:08:47 -0500

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = nil)
+    base_title = "Social Media App"
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
