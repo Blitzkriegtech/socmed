ba7a12352ad660b2429055ecaeffaad315d49ec3
Blitzkriegtech <aljunrendon@yahoo.com>
2026-02-17 11:13:51 +0800

    Finish user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to SOCMED!"
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [ :name, :email, :password, :password_confirmation ])
   end
 end
